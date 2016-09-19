de4a65422b7e696a7bce38a34c05f8e7196beef8
viny76 <jardel.vincent@gmail.com>
2016-09-19 16:47:59 +0200

    Initial Commit

---
+++ ChemineesVallee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChemineesVallee
+//
+//  Created by Vincent Jardel on 19/09/2016.
+//  Copyright © 2016 Vincent Jardel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
